5a8e675caaa80578f92868d5fe2a0bf203a414f7
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-08-27 14:56:15 +0530

    simple todos API key

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
 "use strict";
-const num1Ele = document.getElementById("num1");
-const num2Ele = document.getElementById("num2");
-const btnEle = document.querySelector("button");
-const numResults = [];
-const textResults = [];
-function add(num1, num2) {
-    if (typeof num1 === "number" && typeof num2 === "number")
-        return num1 + num2;
-    else if (typeof num1 === "string" && typeof num2 === "string")
-        return num1 + " " + num2;
-    else
-        +num1 + +num2;
-}
-function printResult(resultObj) {
-    console.log(resultObj.val);
-    return resultObj.timeStamp;
-}
-// console.log(add(1, 6));
-btnEle.addEventListener("click", () => {
-    const num1 = num1Ele.value;
-    const num2 = num2Ele.value;
-    const result = add(+num1, +num2);
-    numResults.push(result);
-    const stringResult = add(num1, num2);
-    textResults.push(stringResult);
-    console.log(result);
-    console.log(stringResult);
-    console.log(printResult({ val: result, timeStamp: new Date() }));
-    console.log(numResults, textResults);
-});
-const myPromise = new Promise((res, rej) => {
-    setTimeout(() => {
-        res("it Worked!");
-    }, 1000);
-});
-myPromise.then((result) => {
-    console.log(result.split(" "));
-});
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const todos_1 = __importDefault(require("./route/todos"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(todos_1.default);
+app.listen(3000);
--- app.ts
+++ app.ts
@@ -1,54 +1,11 @@
-const num1Ele = document.getElementById("num1") as HTMLInputElement;
-const num2Ele = document.getElementById("num2") as HTMLInputElement;
-const btnEle = document.querySelector("button")!;
+import express from "express";
 
-const numResults: Array<number> = [];
-const textResults: string[] = [];
+import todosRoutes from "./route/todos";
 
-type NumOrString = number | string;
-type ResultType = { val: number; timeStamp: Date };
+const app = express();
 
-interface ResultObj {
-  val: number;
-  timeStamp: Date;
-}
+app.use(express.json());
 
-function add(num1: NumOrString, num2: NumOrString) {
-  if (typeof num1 === "number" && typeof num2 === "number") return num1 + num2;
-  else if (typeof num1 === "string" && typeof num2 === "string")
-    return num1 + " " + num2;
-  else +num1 + +num2;
-}
+app.use(todosRoutes);
 
-function printResult(resultObj: ResultObj) {
-  console.log(resultObj.val);
-  return resultObj.timeStamp;
-}
-
-// console.log(add(1, 6));
-
-btnEle.addEventListener("click", () => {
-  const num1 = num1Ele.value;
-  const num2 = num2Ele.value;
-
-  const result = add(+num1, +num2);
-  numResults.push(result as number);
-
-  const stringResult = add(num1, num2);
-  textResults.push(stringResult as string);
-
-  console.log(result);
-  console.log(stringResult);
-  console.log(printResult({ val: result as number, timeStamp: new Date() }));
-  console.log(numResults, textResults);
-});
-
-const myPromise = new Promise<string>((res, rej) => {
-  setTimeout(() => {
-    res("it Worked!");
-  }, 1000);
-});
-
-myPromise.then((result) => {
-  console.log(result.split(" "));
-});
+app.listen(3000);
